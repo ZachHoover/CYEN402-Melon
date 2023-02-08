3df89d8ae2f65ece3c44a0e00b02545f51e63896
Zach Hoover <38825744+ZachHoover@users.noreply.github.com>
2023-02-07 19:12:37 -0600

    Add files via upload

---
+++ Main/Main/Main.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
+#include <ESP8266WebServer.h>
+#include <Keypad.h>
+#include <WebSocketsClient.h>
+#include <AccelStepper.h>
+
+const char* wifissid="Whyphi";
+const char* wifipass="ilikepie";
+
+//Modules
+#include "web.h"
+
+
+void setup() {
+  Serial.begin(9600);
+  web_setup();
+  logs_setup();
+  passwords_setup();
+  stepper_setup();
+}
+
+void loop() {
+  web_loop();
+  passwords_loop();
+  stepper_loop();
+  logs_loop();
+}
---
+++ Main/Main/resistors.h
@@ -0,0 +1,16 @@
+//Globals
+short resistor[5];
+
+
+
+//Setup
+void resistors_setup()
+{
+  resistor[0] = 900;
+}
+
+//Loop
+void resistors_loop()
+{
+  
+}
